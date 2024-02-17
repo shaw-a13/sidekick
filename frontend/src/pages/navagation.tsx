73be2abce4ca159d19e47ccef8a37b9b65e9b393
Aaron Shaw <shaw-a13@ulster.ac.uk>
2024-02-17 20:21:04 +0000

    Adding auth to dynamically render nav

--- frontend/src/pages/navagation.tsx
+++ frontend/src/pages/navagation.tsx
@@ -1,8 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Nav } from "react-bootstrap";
+import { useAuth0 } from '@auth0/auth0-react';
+
+let pages = [
+  {name: 'Home', href: '/', authenticationRequired: false},
+  {name: 'Dashboard', href: '/dashboard', authenticationRequired: true},
+  {name: 'Profile', href: '/profile', authenticationRequired: true},
+]
 
 const Navagation = () => {
+  const { isAuthenticated } = useAuth0();
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container>
@@ -10,8 +18,18 @@ const Navagation = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+        {isAuthenticated ? (
+          pages
+            .map(page => (
+              <Nav.Link>{page.name}</Nav.Link>
+          ))
+        ) : (
+          pages
+            .filter((page) => page.authenticationRequired === false)
+            .map(page => (
+              <Nav.Link>{page.name}</Nav.Link>
+          ))
+        )}
         </Nav>
       </Navbar.Collapse>
     </Container>
