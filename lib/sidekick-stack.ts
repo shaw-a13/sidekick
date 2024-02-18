0f22e80087dd426977adf4d24c4220b5f406b69f
Aaron Shaw <shaw-a13@ulster.ac.uk>
2024-02-18 20:36:53 +0000

    Adding apigateway and dynamo db, populated with dummy data

--- lib/sidekick-stack.ts
+++ lib/sidekick-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3'
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
-import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins'
-import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment'
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudfrontOrigins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 import path = require('path');
 
@@ -85,6 +88,37 @@ export class SidekickStack extends cdk.Stack {
       },
     });
 
+    const sidekickTable = new dynamodb.Table(this, 'sidekickTable', {
+      partitionKey: {
+        name: 'PK',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'SK',
+        type: dynamodb.AttributeType.STRING,
+      }
+    });
+
+    const caseApi = new apiGateway.LambdaRestApi(this, 'caseApi', {
+      handler: sampleLambda,
+      proxy: false,
+      defaultCorsPreflightOptions: {
+        allowOrigins: apiGateway.Cors.ALL_ORIGINS
+      }
+    });
+
+    const cases = caseApi.root.addResource('cases');
+    cases.addMethod('GET');
+
+    const singleCase = cases.addResource('{case}');
+    singleCase.addMethod('GET');
+
+    sampleLambda.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['dynamodb:Scan'],
+      resources: [sidekickTable.tableArn]
+    }))
+
     new cdk.CfnOutput(this, 'CloudFrontURL', {
         value: distribution.domainName,
         description: 'The distribution URL',
@@ -96,5 +130,11 @@ export class SidekickStack extends cdk.Stack {
         description: 'The name of the S3 bucket',
         exportName: 'BucketName',
     });
+
+    new cdk.CfnOutput(this, 'CaseApiUrl', {
+      value: caseApi.url,
+      description: 'The case api URL',
+      exportName: 'CaseApiUrl',
+  });
   }
 }
