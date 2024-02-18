3dc4294110b1acc86309c58da6642b6e3eaacc5d
Aaron Shaw <shaw-a13@ulster.ac.uk>
2024-02-18 20:37:08 +0000

    Adding lambda logic to handle get all cases

--- lambda/sample_lambda/index.py
+++ lambda/sample_lambda/index.py
@@ -1,4 +1,16 @@
+import boto3 
+import json
 
 def handler(event, context):
     print(event)
-    print('hello world')
+    if event['httpMethod'] == 'GET':
+        print('Getting all cases...')
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('SideKickStack-sidekickTableFDBE0BEC-1HYP1C6X8JDIT')
+        response = table.scan()
+        data = response['Items']
+        return {
+            'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+            'body': json.dumps(data)
+            }
+
