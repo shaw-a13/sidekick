dd225533e9d19f05686138267c1714b04ba49e88
Aaron Shaw <shaw-a13@ulster.ac.uk>
2024-06-02 16:45:24 +0100

    Adding auth to nav links

--- frontend/src/pages/navagation.tsx
+++ frontend/src/pages/navagation.tsx
@@ -8,18 +8,48 @@ import logo from "../img/logos/png/logo-no-background.png";
 import { Link } from "react-router-dom";
 
 let pages = [
-  { name: "Home", href: "/", authenticationRequired: false },
-  { name: "Upload", href: "/upload", authenticationRequired: true },
-  { name: "Dashboard", href: "/dashboard", authenticationRequired: true },
-  { name: "Profile", href: "/profile", authenticationRequired: true },
+  {
+    name: "Home",
+    href: "/",
+    authenticationRequired: false,
+    workerRequired: false,
+    adminRequired: false,
+  },
+  {
+    name: "Upload",
+    href: "/upload",
+    authenticationRequired: true,
+    workerRequired: true,
+    adminRequired: false,
+  },
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+    authenticationRequired: true,
+    workerRequired: false,
+    adminRequired: false,
+  },
+  {
+    name: "Profile",
+    href: "/profile",
+    authenticationRequired: true,
+    workerRequired: true,
+    adminRequired: true,
+  },
 ];
 
 const Navagation = () => {
-  const { isAuthenticated } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   return (
-    <Navbar variant="dark" expand="lg" fixed="top" className="shadow-lg" style={{ backgroundColor: "#162836" }}>
+    <Navbar
+      variant="dark"
+      expand="lg"
+      fixed="top"
+      className="shadow-lg"
+      style={{ backgroundColor: "#162836" }}
+    >
       <Container>
-      <Navbar.Brand href="/">
+        <Navbar.Brand href="/">
           <img
             alt=""
             width={"150"}
@@ -30,19 +60,68 @@ const Navagation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" color="#CF7650">
           <Nav className="me-auto">
-            {isAuthenticated
+            {isAuthenticated &&
+            user &&
+            user["authGroups"].includes("Admin", "Worker")
               ? pages.map((page) => (
-                  <Link to={page.href} style={{ color: "#CF7650", textDecoration:"none", margin: 20 }}>
+                  <Link
+                    to={page.href}
+                    style={{
+                      color: "#CF7650",
+                      textDecoration: "none",
+                      margin: 20,
+                    }}
+                  >
                     {page.name}
                   </Link>
                 ))
-              : pages
-                  .filter((page) => page.authenticationRequired === false)
-                  .map((page) => (
-                    <Link to={page.href} style={{ color: "#CF7650", textDecoration:"none", marginTop: 15}}>
-                      <p>{page.name}</p>
-                    </Link>
-                  ))}
+              : isAuthenticated && user && user["authGroups"].includes("Worker")
+                ? pages
+                    .filter((page) => page.adminRequired === false)
+                    .map((page) => (
+                      <Link
+                        to={page.href}
+                        style={{
+                          color: "#CF7650",
+                          textDecoration: "none",
+                          marginTop: 20,
+                          marginRight: 20,
+                        }}
+                      >
+                        <p>{page.name}</p>
+                      </Link>
+                    ))
+                : isAuthenticated && user && user["authGroups"].length === 0
+                  ? pages
+                      .filter((page) => page.adminRequired === false)
+                      .filter((page) => page.workerRequired === false)
+                      .map((page) => (
+                        <Link
+                          to={page.href}
+                          style={{
+                            color: "#CF7650",
+                            textDecoration: "none",
+                            marginTop: 20,
+                            marginRight: 20,
+                          }}
+                        >
+                          <p>{page.name}</p>
+                        </Link>
+                      ))
+                  : pages
+                      .filter((page) => page.authenticationRequired === false)
+                      .map((page) => (
+                        <Link
+                          to={page.href}
+                          style={{
+                            color: "#CF7650",
+                            textDecoration: "none",
+                            marginTop: 15,
+                          }}
+                        >
+                          <p>{page.name}</p>
+                        </Link>
+                      ))}
           </Nav>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
