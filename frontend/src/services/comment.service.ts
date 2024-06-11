14c6aafac67143d8ec50f148ff54ccce5b3cf30c
Aaron Shaw <aaron.shaw@liberty-it.co.uk>
2024-06-11 20:03:56 +0100

    Building history and comment services

--- frontend/src/services/comment.service.ts
+++ frontend/src/services/comment.service.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { BaseService } from "./base.service";
 import { Comment } from "../interfaces/comment/comment.interface";
+import { DynamoEditProps } from "../interfaces/dynamoEditProps.interface";
+import { CommentEditProps } from "../interfaces/comment/commentEditProps.interface";
 
 export class CommentService extends BaseService {
   public async getAllComments(token: string, caseId: string) {
@@ -16,63 +18,48 @@ export class CommentService extends BaseService {
     }
   }
 
-  // public async getSingleComment(token: string, CommentId: string) {
-  //     try {
-  //         return await axios.get<Comment>(`${this.baseUrl}/Comments/${CommentId}`, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
+  public async deleteComment(token: string, caseId: string, commentId: string) {
+    try {
+      return await axios.delete(`${this.baseUrl}/Comments/${caseId}/${commentId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
 
-  // public async deleteComment(token: string, CommentId: string) {
-  //     try {
-  //         return await axios.delete(`${this.baseUrl}/Comments/${CommentId}`, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
+  public async addComment(token: string, commentInfo: Comment) {
+    try {
+      return await axios.post(`${this.baseUrl}/comments`, commentInfo, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
 
-  // public async addComment(token: string, CommentInfo: Comment) {
-  //     try {
-  //         return await axios.post(`${this.baseUrl}/Comments`, CommentInfo, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
-
-  // public async editComment(token: string, CommentEdits: CommentEditProps, CommentId: string) {
-  //     let data: DynamoEditProps = {
-  //         props: []
-  //     }
-  //     for (const [key, value] of Object.entries(CommentEdits)) {
-  //         data.props.push({key, value})
-  //     }
-
-  //     console.log(data)
-  //     try {
-  //         return await axios.put(`${this.baseUrl}/Comments/${CommentId}`, data, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
+  public async editComment(token: string, commentEdits: CommentEditProps, caseId: string, commentId: string) {
+    let data: DynamoEditProps = {
+      props: [],
+    };
+    for (const [key, value] of Object.entries(commentEdits)) {
+      data.props.push({ key, value });
+    }
+    try {
+      return await axios.put(`${this.baseUrl}/Comments/${caseId}/${commentId}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
 }
--- frontend/src/services/history.service.ts
+++ frontend/src/services/history.service.ts
@@ -16,63 +16,16 @@ export class HistoryService extends BaseService {
     }
   }
 
-  // public async getSingleComment(token: string, CommentId: string) {
-  //     try {
-  //         return await axios.get<Comment>(`${this.baseUrl}/Comments/${CommentId}`, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
-
-  // public async deleteComment(token: string, CommentId: string) {
-  //     try {
-  //         return await axios.delete(`${this.baseUrl}/Comments/${CommentId}`, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
-
-  // public async addComment(token: string, CommentInfo: Comment) {
-  //     try {
-  //         return await axios.post(`${this.baseUrl}/Comments`, CommentInfo, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
-
-  // public async editComment(token: string, CommentEdits: CommentEditProps, CommentId: string) {
-  //     let data: DynamoEditProps = {
-  //         props: []
-  //     }
-  //     for (const [key, value] of Object.entries(CommentEdits)) {
-  //         data.props.push({key, value})
-  //     }
-
-  //     console.log(data)
-  //     try {
-  //         return await axios.put(`${this.baseUrl}/Comments/${CommentId}`, data, {
-  //             headers: {
-  //                 'Authorization': `Bearer ${token}`
-  //             }
-  //         });
-  //     } catch (error) {
-  //         console.log('error')
-  //         console.error(error);
-  //     }
-  // }
+  public async addHistory(token: string, caseId: string, historyInfo: History) {
+    try {
+      return await axios.post(`${this.baseUrl}/history/${caseId}`, historyInfo, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
 }
