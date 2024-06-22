0f6cce90e5e0779000edfacba0189322db5e84b9
Aaron Shaw <aaron.shaw@liberty-it.co.uk>
2024-06-22 20:22:12 +0100

    Reformatting

--- frontend/src/services/client.service.ts
+++ frontend/src/services/client.service.ts
@@ -1,97 +1,80 @@
-import axios from 'axios';
-import { Client } from '../interfaces/client/client.interface';
-import { BaseService } from './base.service';
-import { ClientEditProps } from '../interfaces/client/clientEditProps.interface';
-import { DynamoEditProps } from '../interfaces/dynamoEditProps.interface';
+import axios from "axios";
+import { Client } from "../interfaces/client/client.interface";
+import { BaseService } from "./base.service";
+import { ClientEditProps } from "../interfaces/client/clientEditProps.interface";
+import { DynamoEditProps } from "../interfaces/dynamoEditProps.interface";
 
 export class ClientService extends BaseService {
+  public async getAllClients(token: string) {
+    try {
+      return await axios.get<Client[]>(`${this.baseUrl}/clients`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
 
-    public async getAllClients(token: string) {
-        try {
-            return await axios.get<Client[]>(`${this.baseUrl}/clients`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-        } catch (error) {
-            console.log('error')
-            console.error(error);
-        }
+  public async getSingleClient(token: string, clientId: string) {
+    try {
+      return await axios.get<Client>(`${this.baseUrl}/clients/${clientId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
+
+  public async deleteClient(token: string, clientId: string) {
+    try {
+      return await axios.delete(`${this.baseUrl}/clients/${clientId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
+
+  public async addClient(token: string, clientInfo: Client) {
+    try {
+      return await axios.post(`${this.baseUrl}/clients`, clientInfo, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
+    }
+  }
+
+  public async editClient(token: string, clientEdits: ClientEditProps, clientId: string) {
+    let data: DynamoEditProps = {
+      props: [],
+    };
+    for (const [key, value] of Object.entries(clientEdits)) {
+      data.props.push({ key, value });
     }
 
-    public async getSingleClient(token: string, clientId: string) {
-        try {
-            return await axios.get<Client>(`${this.baseUrl}/clients/${clientId}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-        } catch (error) {
-            console.log('error')
-            console.error(error);
-        }
+    console.log(data);
+    try {
+      return await axios.put(`${this.baseUrl}/clients/${clientId}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
     }
-
-    public async deleteClient(token: string, clientId: string) {
-        try {
-            return await axios.delete(`${this.baseUrl}/clients/${clientId}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-        } catch (error) {
-            console.log('error')
-            console.error(error);
-        }
-    }
-
-    public async addClient(token: string, clientInfo: Client) {
-        // const data = {
-        //     firstName: clientInfo.firstName,
-        //     lastName: clientInfo.lastName,
-        //     addressLine1: clientInfo.addressLine1,
-        //     addressLine2: clientInfo.addressLine2,
-        //     postcode: clientInfo.postcode,
-        //     county: clientInfo.county,
-        //     city: clientInfo.city,
-        //     phoneNumber: clientInfo.phoneNumber,
-        //     email: clientInfo.email
-        // }
-
-        console.log(clientInfo)
-
-        try {
-            return await axios.post(`${this.baseUrl}/clients`, clientInfo, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                },
-            });
-        } catch (error) {
-            console.log('error')
-            console.error(error);
-        }
-    }
-
-    public async editClient(token: string, clientEdits: ClientEditProps, clientId: string) {
-        let data: DynamoEditProps = {
-            props: []
-        }
-        for (const [key, value] of Object.entries(clientEdits)) {
-            data.props.push({key, value})
-        }
-
-        console.log(data)
-        try {
-            return await axios.put(`${this.baseUrl}/clients/${clientId}`, data, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-        } catch (error) {
-            console.log('error')
-            console.error(error);
-        }
-    }
-
-
+  }
 }
--- frontend/src/services/document.service.ts
+++ frontend/src/services/document.service.ts
@@ -2,72 +2,69 @@ import axios from "axios";
 import { BaseService } from "./base.service";
 import { DocumentResultResponse } from "../interfaces/document/documentResultResponse.interface";
 
-interface PresignedUrlResponse {
-    presignedUrl: string;
-    key: string;
+export interface PresignedUrlResponse {
+  presignedUrl: string;
+  key: string;
 }
 
-interface ingestionResponse {
-    executionArn: string;
+export interface IngestionResponse {
+  executionArn: string;
 }
-
 
 export class DocumentService extends BaseService {
-
-    public async getPresignedUrl(token: string, caseId: string) {
-        try {
-            return await axios.get<PresignedUrlResponse>(`${this.baseUrl}/upload/${caseId}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                },
-            });
-        } catch (error) {
-            console.log('error')
-            console.error(error);
-        }
+  public async getPresignedUrl(token: string, caseId: string) {
+    try {
+      return await axios.get<PresignedUrlResponse>(`${this.baseUrl}/upload/${caseId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
     }
+  }
 
-    public async uploadDocument(presignedUrl: string, document: any) {
-        try {
-            await axios.put(presignedUrl, document, {
-                headers: {
-                    "Content-Type": "application/pdf",
-                },
-            });
-
-        } catch (error) {
-            console.log('error')
-            console.error(error)
-        }
-
+  public async uploadDocument(presignedUrl: string, document: any) {
+    try {
+      await axios.put(presignedUrl, document, {
+        headers: {
+          "Content-Type": "application/pdf",
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
     }
+  }
 
-    public async getDocuments(token: string, caseId: string) {
-        try {
-            return await axios.get<DocumentResultResponse>(`${this.baseUrl}/download/${caseId}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                },
-            });
-        } catch (error) {
-            console.log('error')
-            console.error(error);
-        }
-
+  public async getDocuments(token: string, caseId: string) {
+    try {
+      return await axios.get<DocumentResultResponse>(`${this.baseUrl}/download/${caseId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.log("error");
+      console.error(error);
     }
+  }
 
-    public async triggerIngestion(token: string, caseId: string, key: string) {
-        try {
-            return await axios.post<ingestionResponse>(`${this.baseUrl}/ingestion`, { caseId: caseId, key: key }, {
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
-
-        } catch (error) {
-            console.log('error')
-            console.error(error)
+  public async triggerIngestion(token: string, caseId: string, key: string) {
+    try {
+      return await axios.post<IngestionResponse>(
+        `${this.baseUrl}/ingestion`,
+        { caseId: caseId, key: key },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-
+      );
+    } catch (error) {
+      console.log("error");
+      console.error(error);
     }
+  }
 }
